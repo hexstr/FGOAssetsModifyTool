95964eb9f71cd5b958798fa18adfc075cd8009ca
nishuoshenme <28940831+nishuoshenme@users.noreply.github.com>
2019-10-25 10:10:52 +0800

    Upload source files

---
+++ FGOAssetsModifyTool/CatAndMouseGame.cs
@@ -0,0 +1,398 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using ICSharpCode.SharpZipLib.BZip2;
+using ICSharpCode.SharpZipLib.GZip;
+
+public class CatAndMouseGame
+{
+    private static byte[] BattleIV = new byte[32];
+    private static byte[] BattleKey = new byte[32];
+    static CatAndMouseGame()
+    {
+        byte[] bytes = Encoding.UTF8.GetBytes("kzdMtpmzqCHAfx00saU1gIhTjYCuOD1JstqtisXsGYqRVcqrHRydj3k6vJCySu3g");
+        byte[] bytes2 = Encoding.UTF8.GetBytes("PFBs0eIuunoxKkCcLbqDVerU1rShhS276SAL3A8tFLUfGvtz3F3FFeKELIk3Nvi4");
+        for (int i = 0; i < bytes2.Length / 4; i++)
+        {
+            if (i % 2 == 0)
+            {
+                CatAndMouseGame.baseData[i / 2 * 4] = bytes2[i * 4];
+                CatAndMouseGame.baseData[i / 2 * 4 + 1] = bytes2[i * 4 + 1];
+                CatAndMouseGame.baseData[i / 2 * 4 + 2] = bytes2[i * 4 + 2];
+                CatAndMouseGame.baseData[i / 2 * 4 + 3] = bytes2[i * 4 + 3];
+            }
+            else
+            {
+                CatAndMouseGame.baseTop[i / 2 * 4] = bytes2[i * 4];
+                CatAndMouseGame.baseTop[i / 2 * 4 + 1] = bytes2[i * 4 + 1];
+                CatAndMouseGame.baseTop[i / 2 * 4 + 2] = bytes2[i * 4 + 2];
+                CatAndMouseGame.baseTop[i / 2 * 4 + 3] = bytes2[i * 4 + 3];
+            }
+        }
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                CatAndMouseGame.stageData[i / 2] = bytes[i];
+            }
+            else
+            {
+                CatAndMouseGame.stageTop[i / 2] = bytes[i];
+            }
+        }
+    }
+    public static byte[] CatGame4(byte[] data)
+    {
+        for (int i = 0; i < data.Length; i += 2)
+        {
+            if (i + 1 >= data.Length)
+            {
+                break;
+            }
+            byte b = data[i];
+            byte b2 = data[i + 1];
+            data[i] = (byte)(b2 ^ 206);
+            data[i + 1] = (byte)(b ^ 210);
+        }
+        return CatAndMouseGame.CatHomeMain(data, CatAndMouseGame.baseData, CatAndMouseGame.baseTop, false);
+    }
+    public static byte[] MouseGame4(byte[] data)
+    {
+        byte[] array = CatAndMouseGame.MouseHomeMain(data, CatAndMouseGame.baseData, CatAndMouseGame.baseTop, false);
+        if (array == null)
+        {
+            Console.WriteLine("MouseHomeMain failed");
+            return null;
+        }
+
+        for (int i = 0; i < array.Length; i += 2)
+        {
+           if (i + 1 >= array.Length)
+           {
+               break;
+           }
+           byte b = array[i];
+           byte b2 = array[i + 1];
+           array[i] = (byte)(b2 ^ 210);
+           array[i + 1] = (byte)(b ^ 206);
+        }
+        return array;
+    }
+    public static string MouseGame8(string str)
+    {
+        byte[] data = Convert.FromBase64String(str);
+        byte[] array = CatAndMouseGame.MouseHomeMainZ2(data, CatAndMouseGame.stageData, CatAndMouseGame.stageTop, true);
+        if (array == null)
+        {
+            array = CatAndMouseGame.MouseHomeMain(data, CatAndMouseGame.stageData, CatAndMouseGame.stageTop, true);
+            if (array == null)
+            {
+                return null;
+            }
+        }
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = (byte)~array[i];
+        }
+        return Encoding.UTF8.GetString(array).TrimEnd(new char[1]);
+    }
+    public static string CatGame8(string str)
+    {
+        byte[] bytes = Encoding.UTF8.GetBytes(str);
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            bytes[i] = (byte)~bytes[i];
+        }
+        return CatAndMouseGame.CatHomeZ2(bytes, CatAndMouseGame.stageData, CatAndMouseGame.stageTop, true);
+    }
+    public static byte[] CatHomeMain(byte[] data, byte[] home, byte[] info, bool isCompress = false)
+    {
+        MemoryStream memoryStream = null;
+        CryptoStream cryptoStream = null;
+        byte[] result;
+        try
+        {
+            ICryptoTransform transform = new RijndaelManaged
+            {
+                Padding = PaddingMode.PKCS7,
+                Mode = CipherMode.CBC,
+                KeySize = 256,
+                BlockSize = 256
+            }.CreateEncryptor(home, info);
+            memoryStream = new MemoryStream();
+            cryptoStream = new CryptoStream(memoryStream, transform, CryptoStreamMode.Write);
+            if (isCompress)
+            {
+                BZip2OutputStream bzip2OutputStream = new BZip2OutputStream(cryptoStream, 1);
+                bzip2OutputStream.Write(data, 0, data.Length);
+                bzip2OutputStream.Close();
+            }
+            else
+            {
+                cryptoStream.Write(data, 0, data.Length);
+                cryptoStream.FlushFinalBlock();
+            }
+            result = memoryStream.ToArray();
+        }
+        catch (Exception)
+        {
+            result = null;
+        }
+        finally
+        {
+            if (memoryStream != null)
+            {
+                memoryStream.Close();
+            }
+            if (cryptoStream != null)
+            {
+                cryptoStream.Close();
+            }
+        }
+        return result;
+    }
+    public static byte[] MouseHomeMainZ2(byte[] data, byte[] home, byte[] info, bool isCompress = false)
+    {
+        MemoryStream memoryStream = null;
+        CryptoStream cryptoStream = null;
+        byte[] result;
+        try
+        {
+            ICryptoTransform transform = new RijndaelManaged
+            {
+                Padding = PaddingMode.PKCS7,
+                Mode = CipherMode.CBC,
+                KeySize = 256,
+                BlockSize = 256
+            }.CreateDecryptor(home, info);
+            byte[] array = new byte[data.Length];
+            memoryStream = new MemoryStream(data);
+            cryptoStream = new CryptoStream(memoryStream, transform, CryptoStreamMode.Read);
+            cryptoStream.Read(array, 0, array.Length);
+            if (isCompress)
+            {
+                if (array[0] == 66 && array[1] == 90)
+                {
+                    return null;
+                }
+                MemoryStream memoryStream2 = new MemoryStream();
+                MemoryStream memoryStream3 = new MemoryStream(array);
+                GZipInputStream gzipInputStream = new GZipInputStream(memoryStream3);
+                byte[] array2 = new byte[16384];
+                int count;
+                while ((count = gzipInputStream.Read(array2, 0, array2.Length)) > 0)
+                {
+                    memoryStream2.Write(array2, 0, count);
+                }
+                gzipInputStream.Close();
+                array = memoryStream2.ToArray();
+                memoryStream3.Close();
+                memoryStream2.Close();
+            }
+            result = array;
+        }
+        catch (Exception)
+        {
+            result = null;
+        }
+        finally
+        {
+            if (memoryStream != null)
+            {
+                memoryStream.Close();
+            }
+            if (cryptoStream != null)
+            {
+                cryptoStream.Close();
+            }
+        }
+        return result;
+    }
+    public static string CatHomeZ2(byte[] data, byte[] home, byte[] info, bool isCompress = false)
+    {
+        byte[] array = CatHomeMainZ2(data, home, info, isCompress);
+        if (array != null)
+        {
+            return Convert.ToBase64String(array);
+        }
+        return null;
+    }
+    public static byte[] CatHomeMainZ2(byte[] data, byte[] home, byte[] info, bool isCompress = false)
+    {
+        MemoryStream memoryStream = null;
+        CryptoStream cryptoStream = null;
+        byte[] result;
+        try
+        {
+            ICryptoTransform cryptoTransform = new RijndaelManaged
+            {
+                Padding = PaddingMode.PKCS7,
+                Mode = CipherMode.CBC,
+                KeySize = 256,
+                BlockSize = 256
+            }.CreateEncryptor(home, info);
+            memoryStream = new MemoryStream();
+            cryptoStream = new CryptoStream(memoryStream, cryptoTransform, CryptoStreamMode.Write);
+            if (isCompress)
+            {
+                GZipOutputStream gzipOutputStream = new GZipOutputStream(cryptoStream);
+                gzipOutputStream.Write(data, 0, data.Length);
+                gzipOutputStream.Close();
+            }
+            else
+            {
+                cryptoStream.Write(data, 0, data.Length);
+                cryptoStream.FlushFinalBlock();
+            }
+            result = memoryStream.ToArray();
+        }
+        catch (Exception)
+        {
+            result = null;
+        }
+        finally
+        {
+            if (memoryStream != null)
+            {
+                memoryStream.Close();
+            }
+            if (cryptoStream != null)
+            {
+                cryptoStream.Close();
+            }
+        }
+        return result;
+    }
+    public static byte[] MouseHomeMain(byte[] data, byte[] home, byte[] info, bool isCompress = false)
+    {
+        byte[] result;
+        try
+        {
+            using (ICryptoTransform cryptoTransform = new RijndaelManaged
+            {
+                Padding = PaddingMode.PKCS7,
+                Mode = CipherMode.CBC,
+                KeySize = 256,
+                BlockSize = 256
+            }.CreateDecryptor(home, info))
+            {
+                using (CatAndMouseGame.DataDecryptor dataDecryptor = new CatAndMouseGame.DataDecryptor(cryptoTransform, data, isCompress))
+                {
+                    dataDecryptor.ApplyWrite();
+                    result = dataDecryptor.ToByteArray();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.StackTrace);
+            result = null;
+        }
+        return result;
+    }
+
+    protected static byte[] stageTop = new byte[32];
+
+    protected static byte[] stageData = new byte[32];
+
+    protected static byte[] baseTop = new byte[32];
+
+    protected static byte[] baseData = new byte[32];
+
+    private class DataDecryptor : IDisposable
+    {
+        public DataDecryptor(ICryptoTransform decryptor, byte[] data, bool isCompress)
+        {
+            this.data = data;
+            this.isCompress = isCompress;
+            this.memoryStream = new MemoryStream(data.Length);
+            this.cryptoStream = new CryptoStream(this.memoryStream, decryptor, CryptoStreamMode.Write);
+        }
+
+        public void ApplyWrite()
+        {
+            this.cryptoStream.Write(this.data, 0, this.data.Length);
+            this.cryptoStream.FlushFinalBlock();
+            if (this.isCompress)
+            {
+                this.memoryStream.Seek(0L, SeekOrigin.Begin);
+                this.memoryStreamBZip = new MemoryStream();
+                this.bzipStream = new BZip2InputStream(this.memoryStream);
+                byte[] array = new byte[16384];
+                int count;
+                while ((count = this.bzipStream.Read(array, 0, array.Length)) > 0)
+                {
+                    this.memoryStreamBZip.Write(array, 0, count);
+                }
+            }
+        }
+
+        public byte[] ToByteArray()
+        {
+            if (!this.isCompress && this.memoryStream != null)
+            {
+                return ((long)this.memoryStream.Capacity != this.memoryStream.Length) ? this.memoryStream.ToArray() : this.memoryStream.GetBuffer();
+            }
+            if (this.memoryStreamBZip != null)
+            {
+                return ((long)this.memoryStreamBZip.Capacity != this.memoryStreamBZip.Length) ? this.memoryStreamBZip.ToArray() : this.memoryStreamBZip.GetBuffer();
+            }
+            Console.Write("memoryStream is null !");
+            return (byte[])Enumerable.Empty<byte>();
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool isDisposing)
+        {
+            if (this.isDisposed)
+            {
+                return;
+            }
+            if (isDisposing)
+            {
+                if (this.memoryStream != null)
+                {
+                    this.memoryStream.Dispose();
+                    this.memoryStream = null;
+                }
+                if (this.cryptoStream != null)
+                {
+                    this.cryptoStream.Dispose();
+                    this.cryptoStream = null;
+                }
+                if (this.memoryStreamBZip != null)
+                {
+                    this.memoryStreamBZip.Dispose();
+                    this.memoryStreamBZip = null;
+                }
+                if (this.bzipStream != null)
+                {
+                    this.bzipStream.Dispose();
+                    this.bzipStream = null;
+                }
+            }
+            this.isDisposed = true;
+        }
+
+        private readonly byte[] data;
+
+        private readonly bool isCompress;
+
+        private MemoryStream memoryStream;
+
+        private CryptoStream cryptoStream;
+
+        private MemoryStream memoryStreamBZip;
+
+        private BZip2InputStream bzipStream;
+
+        private bool isDisposed;
+    }
+}
---
+++ FGOAssetsModifyTool/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+namespace FGOAssetsModifyTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("1: Encrypt\t2:Decrypt\t3: Display_name\n4: Repack\t5: Decrypt AssetStorage.txt\t6: Encrpyt AssetStorage.txt\t7: Export AssetStorage to json\t8: Calculate files crc");
+                int arg = Convert.ToInt32(Console.ReadLine());
+                string path = System.IO.Directory.GetCurrentDirectory();
+                DirectoryInfo folder = new DirectoryInfo(path + @"\com.aniplex.fategrandorder\files\data\d713\");
+                DirectoryInfo needEncrypt = new DirectoryInfo(path + @"\Android\");
+                byte[] raw;
+                byte[] output;
+                switch(arg)
+                {
+                    case 1:
+                        {
+                            foreach (FileInfo file in needEncrypt.GetFiles("*.bin"))
+                            {
+                                Console.WriteLine("Encrypt: " + file.FullName);
+                                raw = File.ReadAllBytes(file.FullName);
+                                output = CatAndMouseGame.CatGame4(raw);
+                                if (!Directory.Exists(path + @"\Encrypt\"))
+                                    Directory.CreateDirectory(path + @"\Encrypt\");
+                                File.WriteAllBytes(path + @"\Encrypt\" + file.Name, output);
+                            }
+                            break;
+                        }
+                    case 2:
+                        {
+                            foreach (FileInfo file in folder.GetFiles("*.bin"))
+                            {
+                                Console.WriteLine("Decrypt: " + file.FullName);
+                                raw = File.ReadAllBytes(file.FullName);
+                                output = CatAndMouseGame.MouseGame4(raw);
+                                if (!Directory.Exists(path + @"\Decrypt\"))
+                                    Directory.CreateDirectory(path + @"\Decrypt\");
+                                File.WriteAllBytes(path + @"\Decrypt\" + file.Name, output);
+                            }
+                            break;
+                        }
+                    case 3:
+                        {
+                            byte[] data = new byte[64];
+                            folder = new DirectoryInfo(path + @"\Decrypt\");
+                            StringBuilder stringBuilder = new StringBuilder("{");
+                            foreach (FileInfo file in folder.GetFiles("*.bin"))
+                            {
+                                Console.WriteLine("Reading file from 'Decrypt': " + file.FullName);
+                                using (var stream = File.OpenRead(file.FullName))
+                                {
+                                    stream.Seek(0x50, SeekOrigin.Begin);
+                                    stream.Read(data, 0, data.Length);
+                                    for (int i = 0; i < 64; ++i)
+                                    {
+                                        if (data[i] == 0x00)
+                                        {
+                                            stringBuilder.Append("{\"hash\":\"" + file.Name + "\",");
+                                            stringBuilder.Append("\"name\":\"" + Encoding.UTF8.GetString(data, 0, i) + "\"},");
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                            string result = stringBuilder.ToString().TrimEnd(',');
+                            result += "}";
+                            File.WriteAllText(path + @"\Android\Name.json", result);
+                            break;
+                        }
+                    case 4:
+                        {
+                            byte[] data = new byte[64];
+                            string filename = "";
+                            folder = new DirectoryInfo(path + @"\Android\");
+                            foreach (FileInfo file in folder.GetFiles("*.bin"))
+                            {
+                                Console.WriteLine("Reading file from: " + file.FullName);
+                                using (var stream = File.OpenRead(file.FullName))
+                                {
+                                    stream.Seek(0x4B, SeekOrigin.Begin);
+                                    stream.Read(data, 0, data.Length);
+                                    for (int i = 0; i < 64; ++i)
+                                    {
+                                        if (data[i] == 0x00)
+                                        {
+                                            filename = Encoding.UTF8.GetString(data, 0, i);
+                                            break;
+                                        }
+                                    }
+                                    stream.Seek(0, SeekOrigin.Begin);
+                                    filename = filename.Replace("CAB-", "");
+                                    Console.WriteLine("Encrypt: " + filename);
+                                    raw = new byte[stream.Length];
+                                    stream.Read(raw, 0, (int)stream.Length);
+                                    output = CatAndMouseGame.CatGame4(raw);
+                                    filename = getShaName(filename + ".unity3d");
+                                    if (!Directory.Exists(path + @"\Modify\"))
+                                        Directory.CreateDirectory(path + @"\Modify\");
+                                    Console.WriteLine("Writing file to: " + path + @"\Modify\" + filename);
+                                    File.WriteAllBytes(path + @"\Modify\" + filename, output);
+                                }
+                            }
+                            break;
+                        }
+                    case 5:
+                        {
+                            string data = File.ReadAllText(path + @"\Android\AssetStorage.txt");
+                            string loadData = CatAndMouseGame.MouseGame8(data);
+                            File.WriteAllText(path + @"\Android\AssetStorage_dec.txt", loadData);
+                            Console.WriteLine("Writing file to: " + path + @"\Android\AssetStorage_dec.txt");
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.WriteLine(@"Read from \Android\AssetStorage_mod.txt");
+                            string loadData = File.ReadAllText(path + @"\Android\AssetStorage_mod.txt");
+                            string tmp;
+                            int num2 = loadData.IndexOfAny(new char[] { '\r','\n' });
+                            if (num2 > 1)
+                            {
+                                string text = loadData.Substring(0, num2);
+                                text = text.Substring(1);
+                                Console.WriteLine("old Crc: " + text);
+                                tmp = loadData.Substring(num2 + 1);
+                                byte[] bytes = Encoding.UTF8.GetBytes(tmp);
+                                uint num3 = Crc32.Compute(bytes);
+                                Console.WriteLine("new Crc: " + num3);
+                                loadData = loadData.Replace(text, num3.ToString());
+                            }
+                            loadData = CatAndMouseGame.CatGame8(loadData);
+                            if (!Directory.Exists(path + @"\Encrypt\"))
+                                Directory.CreateDirectory(path + @"\Encrypt\");
+                            File.WriteAllText(path + @"\Encrypt\AssetStorage.txt", loadData);
+                            Console.WriteLine("Writing file to: " + path + @"\Encrypt\AssetStorage.txt");
+                            break;
+                        }
+                    case 7:
+                        {
+                            Console.WriteLine("Reading file from: " + path + @"\Android\AssetStorage_dec.txt");
+                            string loadData = File.ReadAllText(path + @"\Android\AssetStorage_dec.txt");
+                            string[] listData = null;
+                            loadData = loadData.Trim();
+                            int num2 = loadData.IndexOfAny(new char[] { '\r', '\n' });
+                            loadData = loadData.Substring(num2 + 1);
+                            byte[] bytes = Encoding.UTF8.GetBytes(loadData);
+                            listData = loadData.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                            string[] array = listData[0].Split(',');
+                            Console.WriteLine("Parsing  json...");
+                            StringBuilder stringBuilder = new StringBuilder("{");
+                            int num4;
+                            string attrib;
+                            int size;
+                            uint crc;
+                            string name;
+                            for (int i = 1; i < listData.Length; i++)
+                            {
+                                array = listData[i].Split(',');
+                                if (array.Length != 5)
+                                {
+                                    break;
+                                }
+                                num4 = int.Parse(array[0].Trim());
+                                stringBuilder.Append("{\"num4\":\"" + num4.ToString() + "\",");
+                                attrib = array[1];
+                                stringBuilder.Append("\"attrib\":\"" + attrib.ToString() + "\",");
+                                size = int.Parse(array[2].Trim());
+                                stringBuilder.Append("\"size\":\"" + size.ToString() + "\",");
+                                crc = uint.Parse(array[3].Trim());
+                                stringBuilder.Append("\"crc\":\"" + crc.ToString() + "\",");
+                                name = array[4];
+                                stringBuilder.Append("\"name\":\"" + name + "\"},");
+                            }
+                            string json = stringBuilder.ToString().TrimEnd(',');
+                            json += "}";
+                            File.WriteAllText(path + @"\Android\AssetStorage.json", json);
+                            Console.WriteLine("Writing file to: " + path + @"\Android\AssetStorage.json");
+                            break;
+                        }
+                        case 8:
+                        {
+                            try
+                            {
+                                folder = new DirectoryInfo(path + @"\Encrypt\");
+                                foreach (FileInfo file in folder.GetFiles("*.bin"))
+                                {
+                                    Console.WriteLine(file.Name + "\r\nsize: " + file.Length + "\r\ncrc: " + Crc32.Compute(File.ReadAllBytes(file.FullName)));
+                                    Console.WriteLine("======================================");
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                                Console.WriteLine(ex.StackTrace);
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("3: Export asset bundle name to json\n4: Encrypt and rename to 'ShaName.bin'\n6: Parse item from AssetStorage file");
+                            break;
+                        }
+                }
+                Console.WriteLine("pause...");
+                Console.ReadKey(true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.ReadKey(true);
+            }
+        }
+        public static string getShaName(string name)
+        {
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            UTF8Encoding utf8Encoding = new UTF8Encoding();
+            byte[] bytes = utf8Encoding.GetBytes(name);
+            byte[] array = sha.ComputeHash(bytes);
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (byte b in array)
+            {
+                stringBuilder.AppendFormat("{0,0:x2}", (int)(b ^ 170));
+            }
+            stringBuilder.Append(".bin");
+            return stringBuilder.ToString();
+        }
+    }
+}
